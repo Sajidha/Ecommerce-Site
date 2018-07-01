fa343f771edd691624bd6ed3af1d85476f359e81
Sajidha <b.sajidha@gmail.com>
2018-07-01 22:42:28 +0530

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -9,9 +9,25 @@ $(document).ready(function()
       }, function() {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
       });
+     
+      // var w = $(window).width();
+      // console.log (w);
+      // $('.dropdown-menu').css('width', w);
+      // $('.navbar-right').find('.dropdown-menu').css('width', 'auto');
 
-      var w = $(window).width();
-      $('.dropdown-menu').css('width', w);
-      $('.navbar-right').find('.dropdown-menu').css('width', 'auto');
+      var boxWidth = $(".box").width();
+        $(".slide-left.1").click(function(){
+            $(".box.1").css('left', '0');
+        });
+        $(".slide-right.1").click(function(){
+          $(".box.1").css('left', '100%');
+        });
+
+        $(".slide-left.2").click(function(){
+          $(".box.2").css('left', '0');
+      });
+      $(".slide-right.2").click(function(){
+        $(".box.2").css('left', '100%');
+      });
 
   });
